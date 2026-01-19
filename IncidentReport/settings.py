06b1bb6f0b09b3229a91f97ecd600dd633c1b429
Alyham Almadhaji <alaihemmadhaji@gmail.com>
2026-01-19 14:42:07 +0300

    fixing settings allowed hostes removing https

--- IncidentReport/settings.py
+++ IncidentReport/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','unsafe-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://icident-report.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['icident-report.onrender.com','127.0.0.1']
 
 
 # Application definition
