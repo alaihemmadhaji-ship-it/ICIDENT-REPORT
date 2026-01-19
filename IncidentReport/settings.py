c0cf18c20a461ffd0598e2cfa313a145023910f1
Alyham Almadhaji <alaihemmadhaji@gmail.com>
2026-01-19 15:03:12 +0300

    fxing STATICFILES_STORAGE with wihtenoise

--- IncidentReport/settings.py
+++ IncidentReport/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
